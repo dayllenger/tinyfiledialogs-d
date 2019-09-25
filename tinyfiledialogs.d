9df291cb31db3b0cd2291a81d2cddcb966fa3b4c
dayllenger <dayllenger@protonmail.com>
2019-09-25 06:21:31 +0300

    bring the notes from the header file and delete it

--- tinyfiledialogs.d
+++ tinyfiledialogs.d
@@ -20,6 +20,7 @@ SSH supported via automatic switch to console mode or X11 forwarding.
 
 Each function is documented with examples.
 The dialogs can be forced into console mode.
+Supports UTF-8.
 
 Windows XP to 10:
 - native code & vbs create the graphic dialogs
@@ -34,11 +35,50 @@ Unix (using command line calls):
 - basic console input
 The same executable can run across desktops & distributions.
 
-- Notes -
+*** Notes ***
 
+- Avoid using " and ' in titles and messages.
+- String memory is preallocated statically for all the returned values.
+- Use platform-specific path separators: \ on Windows, / on Unix.
+- If you pass only a path instead of path + filename,
+  make sure it ends with a separator.
+- File and path names are tested before return, they are valid.
+- There's one file filter only, it may contain several patterns.
+- If no filter description is provided,
+  the list of patterns becomes the description.
+- You can query the type of dialog that will be used, see `tinyfd_response`.
 
+- This is not for android nor ios.
+- The code is betterC compatible, originally pure C89.
+- Windows is fully supported from XP to 10 (maybe even older versions).
+- OSX supported from 10.4 to latest (maybe even older versions).
+- On Windows, it links against Comdlg32.lib and Ole32.lib.
+- Set TINYFD_NOLIB version if you don't want to include the code creating
+  graphic dialogs. Then you won't need to link against those libs.
+- On Unix, it tries command line calls.
+  They are already available on most (if not all) desktops.
+- In the absence of those it will use gdialog, gxmessage or whiptail
+  with a textinputbox.
+- If nothing is found, it switches to basic console input,
+  and opens a console if needed (requires xterm + bash).
 
-- License -
+- On Windows, console mode only make sense for console applications.
+- The package dialog must be installed to run in enhanced console mode.
+  It is already installed on most Unix systems.
+- On OSX, the package dialog can be installed via
+  http://macappstore.org/dialog or http://macports.org
+- On Windows, for enhanced console mode,
+  dialog.exe should be copied somewhere on your executable path.
+  It can be found at the bottom of the following page:
+  http://andrear.altervista.org/home/cdialog.php
+- If dialog is missing, it will switch to basic console input.
+- Mutiple selects are not allowed in console mode.
+
+Thanks for contributions, bug corrections & thorough testing to:
+- Don Heyse http://ldglite.sf.net for bug corrections & thorough testing!
+- Paul Rouget
+
+*** License ***
 
 This software is provided 'as-is', without any express or implied
 warranty.  In no event will the authors be held liable for any damages
